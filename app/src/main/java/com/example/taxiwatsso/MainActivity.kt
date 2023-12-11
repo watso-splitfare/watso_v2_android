9c919690cae12faa0a3f800edb9f9abd8901d8b9
hyerin <ohyerin02@naver.com>
2023-12-11 21:08:24 +0900

    home

--- app/src/main/java/com/example/taxiwatsso/MainActivity.kt
+++ app/src/main/java/com/example/taxiwatsso/MainActivity.kt
@@ -3,20 +3,28 @@ package com.example.taxiwatsso
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.taxiwatsso.databinding.ActivityHomeBinding
+import com.example.taxiwatsso.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var mBinding: ActivityHomeBinding? = null
+    private var mBinding: ActivityMainBinding? = null
     // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
     private val binding get() = mBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        mBinding = ActivityHomeBinding.inflate(layoutInflater)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
 
         // getRoot 메서드로 레이아웃 내부의 최상위 위치 뷰의
         // 인스턴스를 활용하여 생성된 뷰를 액티비티에 표시 합니다.
         setContentView(binding.root)
+
+
+
+        //리스트뷰
+        val item = arrayOf("테스트", "시험", "확인중")
+        binding.listview
+
+
     }
 }
